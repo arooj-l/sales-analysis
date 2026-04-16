70707bd1e86f4c31dd4f0e28246092ff2f596cde
arooj-l <aliaqat@udel.edu>
2026-04-16 00:46:12 -0400

    added data analysis using groupby()

--- sales.py
+++ sales.py
@@ -20,3 +20,38 @@ df = pd.DataFrame(data)
 
 # Save the dataFrame to a csv file to simulate a real-world dataset
 df.to_csv("sales_data.csv", index=False) #prevents saving row numbers in the CSV
+
+# Read the CSV file into a new DataFrame
+sales_df = pd.read_csv("sales_data.csv")
+
+# Display the first five rows
+print(sales_df.head())
+
+# Shows column names,  data types, and missig values
+print(sales_df.info())
+
+#Provides mean, min, max, etc
+print(sales_df.describe())
+
+# create a new column "revenue"
+sales_df["Revenue"] = sales_df["Price"] * sales_df["Quantity"]
+
+# Display the updates DataFrame
+print(sales_df)
+
+# Total revenue 
+total_rev = sales_df["Revenue"].sum()
+print(total_rev)
+
+# best selling product
+product_sales = sales_df.groupby("Product")["Quantity"].sum()
+best_product = product_sales.sort_values(ascending=False)
+print(best_product)
+
+# Revenue by Category
+category_revenue = sales_df.groupby("Category")["Revenue"].sum()
+print(category_revenue)
+
+# Average Price per Product
+average_price = sales_df.groupby("Product")["Price"].mean()
+print(average_price)
